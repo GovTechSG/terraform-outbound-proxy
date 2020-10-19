0e22ef1d7ae0cfc86f1b9072566f911ad22ebeed
Ryan Goh <ryangohjq@gmail.com>
2020-10-19 09:58:39 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+locals {
+  tags = {
+    Name      = var.name,
+    Terraform = "True"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+
+resource "aws_cloudformation_stack" "proxy" {
+  name = var.name
+  parameters = {
+    VpcId               = var.vpc_id
+    WhitelistedDomains  = join(",", var.whitelisted_domains)
+    CustomDNS           = var.custom_dns
+    ProxyPort           = var.proxy_port
+    InstanceType        = var.instance_type
+    LatestAmiId         = var.latest_ami_id
+    KeyName             = var.ssh_key_name
+    PermissionsBoundary = var.permissions_boundary
+    PrivateSubnetIDs    = join(",", var.private_subnet_ids)
+    NetworkAllowedCIDR  = var.network_allowed_cidr
+  }
+  template_body = file("./outbound-proxy.yaml")
+}
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "name" {
+  type        = string
+  description = "Name for this cloudformation resource"
+}
+
+variable "whitelisted_domains" {
+  type        = list(string)
+  description = "List of domain names to whitelist for outgoing proxy"
+  default     = [".amazonaws.com", ".debian.org"]
+}
+
+variable "custom_dns" {
+  type        = string
+  description = "Custom DNS servers, set to 'default' to not use a custom dns server"
+  default     = "default"
+}
+
+variable "proxy_port" {
+  type        = number
+  description = "Port to connect to proxy with"
+  default     = 3128
+}
+
+variable "instance_type" {
+  type        = string
+  description = "WebServer EC2 instance type (e.g t3.medium)"
+  default     = "t3.medium"
+}
+
+variable "latest_ami_id" {
+  type        = string
+  description = "AMI ID to use, default to latest AMI Amazon linux2"
+  default     = " /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+variable "ssh_key_name" {
+  type        = string
+  description = "Key pair name to use for ssh key to the instance"
+  default     = ""
+}
+
+variable "permissions_boundary" {
+  type        = string
+  description = "ARN of the IAM policy to apply to iam roles created as permission boundary policy"
+  default     = ""
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private SubnetIDs where the Network LoadBalancer will be placed (Select min 2 max 3)"
+}
+
+variable "network_allowed_cidr" {
+  type        = string
+  description = "CIDR allowed in Proxy Security Group. The allowed block size is between a /32 netmask and /8 netmask"
+  default     = "172.31.0.0/16"
+}
